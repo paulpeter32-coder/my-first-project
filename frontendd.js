733898dcc709761f18da798378f23f70b07cfab6
paulpeter32-coder <petange32@gmail.com>
2026-04-20 05:42:57 -0700

    new update

---
+++ frontendd.js
@@ -0,0 +1,19 @@
+let firstName = "Sam";
+ console.log(firstName);
+
+ firstName = "Samantha";
+console.log(firstName);
+
+// const gee = "Hello";
+// gee = "Hi";
+// console.log(gee);
+let age = 20;
+
+if(age > 18){
+    console.warn("You are underaged");
+}
+else{
+    console.log("You are an adult");
+}
+let isRaining;\
+console.log(isRaining);
